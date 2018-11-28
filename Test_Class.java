2fce4f24c67e82be43fa9a345e0144f10f1458cd
MAndersen990 <mandersen990@gmail.com>
2018-11-28 16:11:08 -0500

    first commit

---
+++ Test_Class.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Test_Class {
+    static Scanner userInput = new Scanner(System.in);
+    static String myName;
+    public static void main(String[]args) {
+
+    }
+}
